a5f3cc4d89ea3d6d943c122d811b50521fb6777c
mm <nathaphong_p@cmu.ac.th>
2022-01-21 15:46:39 +0700

    Finished

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,18 +1,18 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *&p,int N){
+	char *x = new char[N];    
+	for(int i = 0; i < N;i++) x[i] = 'A'+i;
+	p = &x[0];
 }
 
 int main(){
 	char *p;
 	int N;	
 	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
+	cin >> N;
+	myString(p,N);		
 	cout << p;	
 	delete [] p;
 	return 0;
--- lab18_3.cpp
+++ lab18_3.cpp
@@ -21,4 +21,23 @@ int main(){
 	return 0;
 }
 
-//Write Definition of randomVector() showVector() and dotProduct() here
+vector<int> randomVector(unsigned int N){	
+    vector<int> v;
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
+}
+
+void showVector(vector<int> v){
+	cout << "[";
+	for(unsigned int i = 0; i < v.size(); i++){
+		cout << v[i];
+		if(i+1 == v.size()) cout << "]";
+		else cout << " ";
+	}
+}
+
+int dotProduct(vector<int> a, vector<int> b){
+	int sum = 0;
+	for(unsigned int i = 0;i < a.size(); i++) sum += (a[i] * b[i]);	
+	return sum;
+}
